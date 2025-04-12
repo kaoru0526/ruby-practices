801fd13b0729f2e7971f15e8a68dbb2dc62e161b
“kaoru.am” <intel.uuuk@gmail.com>
2025-04-12 19:46:27 +0900

    lsコマンドを作成

---
+++ 04.ls/ls.rb
@@ -0,0 +1,15 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: true
+
+def list_files(directory, columns)
+  files = Dir.entries(directory) - ['.', '..']
+  print_files(files, columns)
+end
+
+def print_files(files, columns)
+  files.each_slice(columns) do |slice|
+    puts slice.map { |file| file.ljust(20) }.join(' ')
+  end
+end
+
+list_files(Dir.pwd, 3)
