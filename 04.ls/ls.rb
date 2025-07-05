3460d9fa84d4ca2f2a309a702c82b4e93c01cfcb
“kaoru.am” <intel.uuuk@gmail.com>
2025-07-05 17:08:42 +0900

    ls.rb の修正を反映

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,13 +2,15 @@
 # frozen_string_literal: true
 
 def list_files(directory, columns)
-  files = Dir.entries(directory) - ['.', '..']
+  files = Dir.entries(directory).sort - ['.', '..']
   print_files(files, columns)
 end
 
 def print_files(files, columns)
-  files.each_slice(columns) do |slice|
-    puts slice.map { |file| file.ljust(20) }.join(' ')
+  rows = (files.size + columns - 1) / columns
+  padded = files + [''] * (rows * columns - files.size)
+  padded.each_slice(rows).to_a.transpose.each do |row|
+    puts row.map { |f| f.ljust(20) }.join
   end
 end
 
