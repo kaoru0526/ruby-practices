48b245e0d1f29d047ef2953084ff27944001a108
“kaoru.am” <intel.uuuk@gmail.com>
2025-07-19 21:04:39 +0900

    ls2:rubocopの指示で後置ifへ変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,6 +3,9 @@
 
 require 'optparse'
 
+TAB_WIDTH = 8
+COLUMNS = 3
+
 options = {}
 opt = OptionParser.new
 
@@ -12,4 +15,26 @@ end
 
 opt.parse!(ARGV)
 
-puts "オプション: #{options}"
+def list_files(directory, columns, show_hidden_files: false)
+  files = Dir.entries(directory)
+  files.reject! { |f| f.start_with?('.') } unless show_hidden_files
+
+  files.sort!
+  print_files(files, columns)
+end
+
+def print_files(files, columns)
+  return if files.empty?
+
+  max_length = files.map(&:size).max
+  column_width = ((max_length + TAB_WIDTH) / TAB_WIDTH) * TAB_WIDTH
+
+  rows = (files.size + columns - 1) / columns
+  padded_files = files + [''] * (rows * columns - files.size)
+
+  padded_files.each_slice(rows).to_a.transpose.each do |row|
+    puts row.map { |f| f.ljust(column_width) }.join
+  end
+end
+
+list_files(Dir.pwd, COLUMNS, show_hidden_files: options[:all])
