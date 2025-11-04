d7206c162c86dac65a078c20cc833516ac8c5477
“kaoru.am” <intel.uuuk@gmail.com>
2025-11-04 23:18:42 +0900

    wc:提出物の修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,20 +3,23 @@
 
 require 'optparse'
 
-def parse_options!(argv)
+def parse_options_from_argv(argv)
   opts = { l: false, w: false, c: false }
 
-  OptionParser.new do |o|
+  parser = OptionParser.new do |o|
     o.on('-l', 'lines')  { opts[:l] = true }
     o.on('-w', 'words')  { opts[:w] = true }
     o.on('-c', 'bytes')  { opts[:c] = true }
-  end.parse!(argv)
+  end
+
+  remaining_filenames = parser.parse(argv)
 
   opts = opts.transform_values { true } if opts.values.none?
-  opts
+
+  [opts, remaining_filenames]
 end
 
-def count_lwb(content)
+def count_contents(content)
   {
     l: content.count("\n"),
     w: content.split.size,
@@ -26,51 +29,54 @@ end
 
 def sum_counts(results)
   totals = { l: 0, w: 0, c: 0 }
-  results.each do |r|
-    totals[:l] += r[:counts][:l]
-    totals[:w] += r[:counts][:w]
-    totals[:c] += r[:counts][:c]
+  results.each do |result|
+    totals[:l] += result[:counts][:l]
+    totals[:w] += result[:counts][:w]
+    totals[:c] += result[:counts][:c]
   end
   totals
 end
 
 MIN_FIELD_WIDTH = 8
 
-def max_widths(keys, rows, sum_counts: nil, min_width: MIN_FIELD_WIDTH)
-  all = rows.map { |r| r[:counts] }
+def max_widths(enabled_options, rows, sum_counts: nil, min_width: MIN_FIELD_WIDTH)
+  all = rows.map { |result| result[:counts] }
   all << sum_counts if sum_counts
-  keys.to_h do |k|
-    max_digits = all.map { |h| h[k].to_s.size }.max || 1
-    [k, [max_digits, min_width].max]
+  enabled_options.to_h do |flag|
+    max_digits = all.map { |h| h[flag].to_s.size }.max || 1
+    [flag, [max_digits, min_width].max]
   end
 end
 
-def format_line(keys, widths, counts, name = nil)
-  fields = keys.map { |k| counts[k].to_s.rjust(widths[k]) }.join(' ')
+def format_line(enabled_options, widths, counts, name = nil)
+  fields = enabled_options.map { |flag| counts[flag].to_s.rjust(widths[flag]) }.join(' ')
   name ? "#{fields} #{name}" : fields
 end
 
-opts = parse_options!(ARGV)
-keys = %i[l w c].select { |k| opts[k] }
+options, filenames = parse_options_from_argv(ARGV.dup)
+enabled_options = %i[l w c].select { |flag| options[flag] }
 
-if ARGV.empty?
-  data = $stdin.read
-  stdin_counts = count_lwb(data)
+inputs = if filenames.empty?
+           content = $stdin.read
+           [{ name: nil, content: content }]
+         else
+           filenames.map do |name|
+             { name: name, content: File.binread(name) }
+           end
+         end
 
-  widths = max_widths(keys, [{ counts: stdin_counts }])
-  puts format_line(keys, widths, stdin_counts)
-
-else
-  results = ARGV.map do |name|
-    content = File.binread(name)
-    { name: name, counts: count_lwb(content) }
-  end
-
-  totals = results.size >= 2 ? sum_counts(results) : nil
-  widths = max_widths(keys, results, sum_counts: totals)
-
-  results.each do |r|
-    puts format_line(keys, widths, r[:counts], r[:name])
-  end
-  puts format_line(keys, widths, totals, 'total') if totals
+results = inputs.map do |input|
+  {
+    name: input[:name],
+    counts: count_contents(input[:content])
+  }
 end
+
+totals = results.size >= 2 ? sum_counts(results) : nil
+widths = max_widths(enabled_options, results, sum_counts: totals)
+
+results.each do |result|
+  puts format_line(enabled_options, widths, result[:counts], result[:name])
+end
+
+puts format_line(enabled_options, widths, totals, 'total') if totals
